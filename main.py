4a33ecab42343f72a4a50be0c66df139b463e4f3
Yusuf Ali <yousefmonsteer1234m@gmail.com>
2024-09-28 22:17:35 +0300

    Player drawing with added move function

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
-import constants
-
+from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_HEIGHT, constants.SCREEN_WIDTH))
+    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+    time = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         events = pygame.event.get()
@@ -13,7 +16,11 @@ def main():
                 return
 
         screen.fill("Black")
+        player.draw(screen)
         pygame.display.flip()
+        
+    
+        dt = time.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
